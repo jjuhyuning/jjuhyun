5c8d2a13f663b98db7932320b3315f54056d05b3
jjuhyuning <jh950209@gmail.com>
2025-06-02 16:00:24 +0900

    250602_test

---
+++ jjuhyun/src/main/java/com/itwillbs/JjuhyunApplication.java
@@ -0,0 +1,13 @@
+package com.itwillbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjuhyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjuhyunApplication.class, args);
+	}
+
+}
